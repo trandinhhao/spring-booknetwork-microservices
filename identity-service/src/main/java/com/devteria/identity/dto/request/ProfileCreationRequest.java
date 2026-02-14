ccd97ba5b7ea508a2672151cec4dacb964dda3ed
trandinhhao <haodinhtran06@gmail.com>
2026-02-15 01:31:54 +0700

    feat(profile): calling profile-service when create user using identity-service

--- identity-service/src/main/java/com/devteria/identity/dto/request/ProfileCreationRequest.java
+++ identity-service/src/main/java/com/devteria/identity/dto/request/ProfileCreationRequest.java
@@ -1,15 +1,16 @@
-package com.devteria.profile.dto.request;
+package com.devteria.identity.dto.request;
 
+import com.devteria.identity.validator.DobConstraint;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProfileCreationRequest {
     String userId;
