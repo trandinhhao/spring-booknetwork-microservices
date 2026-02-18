3d3565899877770c986d2f5b61c4c36c2a83e72c
trandinhhao <haodinhtran06@gmail.com>
2026-02-19 01:40:35 +0700

    feat(authen+author): impl between micro-services and add public_endpoint

--- identity-service/src/main/java/com/booknetwork/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/booknetwork/identity/repository/httpclient/ProfileClient.java
@@ -1,5 +1,6 @@
 package com.booknetwork.identity.repository.httpclient;
 
+import com.booknetwork.identity.configuration.AuthenticationRequestInterceptor;
 import com.booknetwork.identity.dto.request.ApiResponse;
 import com.booknetwork.identity.dto.request.ProfileCreationRequest;
 import com.booknetwork.identity.dto.response.UserProfileResponse;
@@ -8,7 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(name = "profile-service", url = "${app.services.profile}",
+        configuration = {AuthenticationRequestInterceptor.class}) // impl cai nay cho feign-client
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     ApiResponse<UserProfileResponse> createProfile(@RequestBody ProfileCreationRequest request);
--- profile-service/src/main/java/com/booknetwork/profile/configuration/CustomJwtDecoder.java
+++ profile-service/src/main/java/com/booknetwork/profile/configuration/CustomJwtDecoder.java
@@ -13,20 +13,6 @@ public class CustomJwtDecoder implements JwtDecoder {
 
     @Override
     public Jwt decode(String token) throws JwtException {
-//        var response = authenticationService.introspect(
-//                IntrospectRequest.builder().token(token).build());
-//
-//        if (!response.isValid()) throw new JwtException("Token invalid");
-//
-//        if (Objects.isNull(nimbusJwtDecoder)) {
-//            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
-//            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
-//                    .macAlgorithm(MacAlgorithm.HS512)
-//                    .build();
-//        }
-//
-//        return nimbusJwtDecoder.decode(token);
-
         try {
             SignedJWT signedJWT = SignedJWT.parse(token);
 
