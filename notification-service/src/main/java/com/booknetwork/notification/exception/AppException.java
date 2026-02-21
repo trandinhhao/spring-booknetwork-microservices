c5219ef6cb86f6cadbf0e436b9999ced959393d1
trandinhhao <haodinhtran06@gmail.com>
2026-02-21 13:55:26 +0700

    feat(notification): impl code-base for notification-service

---
+++ notification-service/src/main/java/com/booknetwork/notification/exception/AppException.java
@@ -0,0 +1,21 @@
+package com.booknetwork.notification.exception;
+
+import com.booknetwork.profile.exception.ErrorCode;
+
+public class AppException extends RuntimeException {
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    private ErrorCode errorCode;
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
